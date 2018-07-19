988083b35bfabd1e260d12e4e18033c128030c96
tyneprincesa <j.nakitto@powem.net>
2018-07-19 05:11:03 +0300

    entries and user-profile pages

---
+++ APIs/api.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template,jsonify
+app = Flask(__name__)
+
+entries = [
+    {"entry1":"content1"},
+    {"entry2":"content2"},
+    {"entry3":"content3"}]
+
+@app.route("/entries", methods=["GET"])
+def entry():
+    abc = [x for a in entries for x in a.keys()]
+    return jsonify({"entries":abc})
+
+@app.route("/entries/<string:entryId>")
+def ttt(entryId):
+    for x in entries:
+        for a,b in x.iteritems():
+            if a == entryId:
+                return b
+    return jsonify(b)
+    
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
