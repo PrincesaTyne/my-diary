3a6ccc76b34e5cd612b6c477525d1759140a95e6
tyneprincesa <j.nakitto@powem.net>
2018-07-21 00:11:07 +0300

    changed files

--- apis/tests/test.py
+++ apis/tests/test.py
@@ -1,4 +1,24 @@
-import requests, json
+import requests, json, unittest
 
 response = requests.get("/entries")
 print (json.dumps(response.json(), indent=4))
+
+class Test1(unittest.TestCase):
+    @classmethod
+    def setUpClass(Cls):
+        print "runs before setup mthd"
+    @classmethod
+    def tearDownClass(Cls):
+        print "runs after setup mthd"
+    
+    def setUp(self):
+        print "runs at the begining of testing"
+    
+    def tearDown(self):
+        print "runs at the end of testing"
+
+    def test_exercise(self):
+        print "i am another test"
+
+    def test_sample(self):
+        print "i am a sample test"
