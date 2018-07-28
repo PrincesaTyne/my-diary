fc7071e899f47820679a49d247eec679b68b5db9
tyneprincesa <j.nakitto@powem.net>
2018-07-28 11:24:02 +0300

    test delete endpoint

--- tests/test_now.py
+++ tests/test_now.py
@@ -26,66 +26,70 @@ edited_data = {'title':'work',
             'date':'1/1/2000'}
 
 class TestEntries(unittest.TestCase):
+    """ Tests the different endpoints in app.py """
     def setUp(self):
-        """Runs prior to each test and setups any state specific to
-         the execution of the given module."""
+        """ Runs prior to each test and setups any state specific to
+         the execution of the given module. """
         self.client = app.test_client(self)
 
 
 
     def tearDown(self):
-        """Runs after each test and teardowns any state that was 
+        """ Runs after each test and teardowns any state that was 
         previously setup with a setup module
         method.
         """
         entries[:] = []
         print("Tests are done")
     
-    #Test whether entries is empty
     def test_get_empty_entry(self):
+        """ Tests get_all_eentries when entries list is empty. """        
         response = self.client.get('/api/v1/entries')
         print (response)
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"There are no entries yet", response.data)
     
-    #Test for successfully returning all entries
     def test_get_all_entries(self):
+        """ Tests for success returning all entries. """
         self.client.post("/api/v1/entries",
                         data = json.dumps(dummy),
                         content_type = 'application/json')
         response = self.client.get('/api/v1/entries')
         self.assertEqual(response.status_code, 200)
     
-    #Test whether entries is empty
     def test_get_single_empty_entry(self):
+        """ Tests get_single_entry when entries list is empty. """
         response = self.client.get('/api/v1/entries/4')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"There are no entries yet", response.data)
     
-    #Test whther get single entry is a success
     def test_get_single_entry(self):
+        """  Tests for get_single_entry success. """
         self.client.post("/api/v1/entries",
                         data = json.dumps(dummy),
                         content_type = 'application/json')
         response = self.client.get("/api/v1/entries/4")
         self.assertEqual(response.status_code, 200)
     
-    #Test edit_entry when entries is empty
     def test_edit_empty_entry(self):
+        """ Tests edit_entry when entries list is empty. """
         response = self.client.put('/api/v1/entries/4')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"There are no entries in your diary", response.data)
-    
-    #Test edit_entry success
+
     def test_edit_entry(self):
+        """ Tests edit_entry success. """
         self.client.post("/api/v1/entries",
                         data = json.dumps(dummy),
                         content_type = 'application/json')
-        response = self.client.put('/api/v1/entries/4')
+        response = self.client.put("/api/v1/entries/4",
+                                    data = json.dumps(edited_data),
+                                    content_type = 'application/json')                
         self.assertEqual(response.status_code, 200)
+
     
-    #Test add_entry check whether key inputs are are valid
     def test_add_entry_valid_inputs(self):
+        """ Tests add_entry when key inputs are invalid. """
         self.client.post("/api/v1/entries",
                         data = json.dumps(invalid_entry),
                         content_type = 'application/json')
@@ -96,18 +100,18 @@ class TestEntries(unittest.TestCase):
     
         #Test add_entry check whether id already exists
     def test_add_entry_id_exists(self):
+        """ Tests add_entry when id already exists. """
         self.client.post("/api/v1/entries",
                         data = json.dumps(dummy),
                         content_type = 'application/json')
         response=self.client.post("/api/v1/entries",
                         data = json.dumps(dummy),
                         content_type = 'application/json')
-        # response = self.client.get('/api/v1/entries')
-        print(response)
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"That id is already taken",response.data)
     
     def test_add_entry_null_entry(self):
+        """ Tests add_entry when values added are null. """
         response = self.client.post("/api/v1/entries",
                         data = json.dumps(null_entry),
                         content_type = 'application/json')
@@ -115,22 +119,27 @@ class TestEntries(unittest.TestCase):
         self.assertIn(b"One of the fields is empty",response.data)
 
     def test_add_entry_success(self):
+        """ Tests add_entry success. """
         response = self.client.post("/api/v1/entries",
                         data = json.dumps(dummy),
                         content_type = 'application/json')
         self.assertEqual(response.status_code, 201)
         self.assertIn(b'went to work yesterday',response.data)
     
-    def test_edit_entry_valid(self):
+        
+    def test_delete_entry_empty(self):
+        """ Tests delete_entry when entries list is empty. """
+        response = self.client.delete('/api/v1/entries/4')
+        self.assertEqual(response.status_code, 200)
+
+        
+    def test_delete_entry(self):
+        """  Tests for delete_entry success. """
         self.client.post("/api/v1/entries",
                         data = json.dumps(dummy),
                         content_type = 'application/json')
-        response = self.client.put("/api/v1/entries/4",
-                                    data = json.dumps(edited_data),
-                                    content_type = 'application/json')                
+        response = self.client.delete('/api/v1/entries/4')
         self.assertEqual(response.status_code, 200)
-        # print(response)
-        # self.assertIn(b'went to work yesterday',response.data)
 
     
     if __name__ == '__main__':
